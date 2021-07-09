ecfe38f896d041a23091810d0bfc4736a829ec38
Michael Harnett <62779855+MichaelHarnett@users.noreply.github.com>
2021-07-09 10:55:02 -0400

    Add files via upload

---
+++ app_code.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import numpy as np 
+import pandas as pd
+import tensorflow as tf
+from tensorflow import keras
+from keras import preprocessing, layers
+from keras.preprocessing.image import image_dataset_from_directory, ImageDataGenerator, load_img
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+from PIL import Image
+from classify import photo_tester
+from io import BytesIO, StringIO
+
+
+model = tf.keras.models.load_model('Data/my_model') # saved from colab
+df = pd.read_csv('data/recipie_df.csv') # dataframe of recipies and ingredients
+
+
+
+uploaded_file = st.file_uploader("Choose an image...", type=['jpg','png'])
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    img = uploaded_file.getvalue()
+    st.image(image, caption='Uploaded Image.', use_column_width=True)
+    st.write("")
+    st.write("Classifying...")
+    classification = photo_tester(image)
---
+++ classify.py
@@ -0,0 +1,66 @@
+import streamlit as st
+import numpy as np 
+import pandas as pd
+import tensorflow as tf
+from tensorflow import keras
+from keras import preprocessing, layers
+from keras.preprocessing.image import image_dataset_from_directory, ImageDataGenerator, image
+from keras.preprocessing.image import image, load_img, img_to_array
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+from PIL import Image
+
+
+model = tf.keras.models.load_model('Data/my_model') # saved from colab
+df = pd.read_csv('data/recipie_df.csv') # dataframe of recipies and ingredients
+
+
+
+label_names = [
+ 'Aperol Spritz',
+ 'Bloody Mary',
+ 'Cosmo',
+ 'Irish Coffee',
+ 'Margarita',
+ 'Mimosa',
+ 'Mojito',
+ 'Moscow Mule',
+ 'Old Fashioned',
+ 'Pina Colada']
+
+
+
+
+def photo_tester(photo):
+    '''
+    This function is designed to first, pre-process the uploaded image to be commpatible with the nerual net results.
+    It then predicts the top 3 classes it belongs to, and provieds a photo ingredients and how to make the highest
+    probable class
+    '''
+    #pre-process
+
+    #photo = image.resize((256,256,3))
+    x = image.img_to_array(photo)
+    x = np.expand_dims(x, axis=0)
+    
+#     #display
+#     img = mpimg.imread(path)
+#     imgplot = plt.imshow(img)
+#     plt.show()
+    
+    backwards = model.predict(x).argsort()[0]
+    forwards = []
+    for items in backwards[-1::-1]:
+        forwards.append(label_names[items])
+        
+    st.write('We believe it looks like like this!')
+    st.write(forwards[0])
+    
+    ingredients = df.loc[df.cocktail == forwards[0]]['ingredients'].tolist()[0]
+    st.write('The ingredients for this cocktail are:' + ingredients)
+    
+    recipie = df.loc[df.cocktail == forwards[0]]['recipie'].tolist()[0]
+    st.write('And you make it like this!' + recipie)
+    
+    
+    st.write('And if that\'s not it, it may be one of these:' + forwards[1] + forwards[2] )
